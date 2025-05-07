6446697fe4cc3517b07e67f0083a9b47020aef4b
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2025-05-06 22:04:56 -0600

    Printer methods are void; Buffer and Builder expose read-only innards.

--- printing.go
+++ printing.go
@@ -7,48 +7,46 @@ import (
 	"strings"
 )
 
-type printer struct {
-	writer io.Writer
+// Printer is the main value-add of this package: types with print methods.
+// This printer's methods, unlike the fmt methods with corresponding names,
+// do not return the number of bytes written nor an error value. Indeed,
+// any error values generated are discarded. You have been warned.
+type Printer interface {
+	Printf(format string, args ...any)
+	Println(args ...any)
+	Print(args ...any)
 }
+
+type printer struct{ io.Writer }
 
 // NewPrinter wraps an io.Writer, exposing Print, Printf, and Println methods.
-func NewPrinter(writer io.Writer) Printer {
-	return &printer{writer: writer}
-}
+func NewPrinter(w io.Writer) Printer { return &printer{w} }
 
-func (this *printer) Printf(format string, args ...any) (int, error) {
-	return fmt.Fprintf(this.writer, format, args...)
-}
-func (this *printer) Println(args ...any) (int, error) {
-	return fmt.Fprintln(this.writer, args...)
-}
-func (this *printer) Print(args ...any) (int, error) {
-	return fmt.Fprint(this.writer, args...)
-}
+func (this *printer) Printf(f string, a ...any) { _, _ = fmt.Fprintf(this.Writer, f, a...) }
+func (this *printer) Println(a ...any)          { _, _ = fmt.Fprintln(this.Writer, a...) }
+func (this *printer) Print(a ...any)            { _, _ = fmt.Fprint(this.Writer, a...) }
 
-type buffer struct {
-	*bytes.Buffer
+type Buffer struct {
+	inner *bytes.Buffer
 	Printer
 }
 
 // NewBuffer is a near drop-in replacement for bytes.NewBuffer(), and exposes the result as a printer.
-func NewBuffer(content []byte) Buffer {
-	b := bytes.NewBuffer(content)
-	return &buffer{Buffer: b, Printer: NewPrinter(b)}
-}
+func NewBuffer(c []byte) *Buffer { b := bytes.NewBuffer(c); return &Buffer{b, NewPrinter(b)} }
 
 // NewBufferString is a near drop-in replacement for bytes.NewBufferString(), and exposes the result as a printer.
-func NewBufferString(content string) Buffer {
-	return NewBuffer([]byte(content))
-}
+func NewBufferString(c string) *Buffer { return NewBuffer([]byte(c)) }
 
-type builder struct {
-	*strings.Builder
+// Inner provides read-only access to the underlying *bytes.Buffer.
+func (this *Buffer) Inner() *bytes.Buffer { return this.inner }
+
+type Builder struct {
+	inner *strings.Builder
 	Printer
 }
 
 // NewBuilder is a near drop-in replacement for new(strings.Builder) and exposes the result as a printer.
-func NewBuilder() Builder {
-	b := new(strings.Builder)
-	return &builder{Builder: b, Printer: NewPrinter(b)}
-}
+func NewBuilder() *Builder { b := new(strings.Builder); return &Builder{b, NewPrinter(b)} }
+
+// Inner provides read-only access to the underlying *strings.Builder.
+func (this *Builder) Inner() *strings.Builder { return this.inner }
--- printing_test.go
+++ printing_test.go
@@ -10,36 +10,36 @@ import (
 func ExampleNewPrinter() {
 	var buffer bytes.Buffer
 	printer := printing.NewPrinter(&buffer)
-	_, _ = printer.Print("hello,")
-	_, _ = printer.Printf(" %s", "world")
-	_, _ = printer.Println("!")
+	printer.Print("hello,")
+	printer.Printf(" %s", "world")
+	printer.Println("!")
 	fmt.Println(buffer.String())
 	// Output: hello, world!
 }
 
 func ExampleNewBuffer() {
 	buffer := printing.NewBuffer([]byte("(initial) "))
-	_, _ = buffer.Print("hello,")
-	_, _ = buffer.Printf(" %s", "world")
-	_, _ = buffer.Println("!")
-	fmt.Println(buffer.String())
+	buffer.Print("hello,")
+	buffer.Printf(" %s", "world")
+	buffer.Println("!")
+	fmt.Println(buffer.Inner().String())
 	// Output: (initial) hello, world!
 }
 
 func ExampleNewBufferString() {
 	buffer := printing.NewBufferString("(initial) ")
-	_, _ = buffer.Print("hello,")
-	_, _ = buffer.Printf(" %s", "world")
-	_, _ = buffer.Println("!")
-	fmt.Println(buffer.String())
+	buffer.Print("hello,")
+	buffer.Printf(" %s", "world")
+	buffer.Println("!")
+	fmt.Println(buffer.Inner().String())
 	// Output: (initial) hello, world!
 }
 
 func ExampleNewBuilder() {
 	builder := printing.NewBuilder()
-	_, _ = builder.Print("hello,")
-	_, _ = builder.Printf(" %s", "world")
-	_, _ = builder.Println("!")
-	fmt.Println(builder.String())
+	builder.Print("hello,")
+	builder.Printf(" %s", "world")
+	builder.Println("!")
+	fmt.Println(builder.Inner().String())
 	// Output: hello, world!
 }
